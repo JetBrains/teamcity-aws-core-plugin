6ba18b083c1ab303ef8f12d90502ab33f3b573fc
Dmitrii Bogdanov <dmitrii.bogdanov@jetbrains.com>
2020-11-26 21:05:51 +0100

    TW-67582,TW-66202 Missing class

---
+++ aws-core-common/src/main/java/jetbrains/buildServer/util/amazon/retry/impl/NoRetryRetrierImpl.java
@@ -0,0 +1,19 @@
+package jetbrains.buildServer.util.amazon.retry.impl;
+
+import java.util.concurrent.Callable;
+import org.jetbrains.annotations.NotNull;
+
+public class NoRetryRetrierImpl extends RetrierImpl {
+  public NoRetryRetrierImpl() {
+    super(0);
+  }
+
+  @Override
+  public <T> T execute(@NotNull Callable<T> callable) {
+    try {
+      return callable.call();
+    } catch (Exception e) {
+      throw e instanceof RuntimeException ? (RuntimeException)e : new RuntimeException(e.getMessage(), e);
+    }
+  }
+}

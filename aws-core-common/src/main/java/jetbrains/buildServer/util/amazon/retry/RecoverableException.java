f46de16601c90bbb42f37c2760a02129c72a3e21
Dmitrii Bogdanov <dmitrii.bogdanov@jetbrains.com>
2021-10-04 17:12:10 +0200

    [s3] Make fetchExceptions possibly recoverable

--- aws-core-common/src/main/java/jetbrains/buildServer/util/amazon/retry/RecoverableException.java
+++ aws-core-common/src/main/java/jetbrains/buildServer/util/amazon/retry/RecoverableException.java
@@ -1,5 +1,6 @@
 package jetbrains.buildServer.util.amazon.retry;
 
+import jetbrains.buildServer.util.ExceptionUtil;
 import org.jetbrains.annotations.NotNull;
 
 public abstract class RecoverableException extends RuntimeException {
@@ -9,6 +10,14 @@ public abstract class RecoverableException extends RuntimeException {
 
   public RecoverableException(@NotNull String message, @NotNull Throwable cause) {
     super(message, cause);
+  }
+
+  protected static boolean isRecoverable(@NotNull final Throwable cause) {
+    final RecoverableException recoverableException = ExceptionUtil.getCause(cause, RecoverableException.class);
+    if (recoverableException != null) {
+      return recoverableException.isRecoverable();
+    }
+    return false;
   }
 
   public abstract boolean isRecoverable();

43d116291f331c0fc600da9dd0be5fa444c6cee0
andrey.yuldashev <andrey.yuldashev@jetbrains.com>
2022-04-01 14:59:05 +0300

    Add check for empty AWS Connections to expose case.

--- aws-core-server/src/main/java/jetbrains/buildServer/clouds/amazon/connector/buildFeatures/envVars/InjectAwsCredsToEnvVars.java
+++ aws-core-server/src/main/java/jetbrains/buildServer/clouds/amazon/connector/buildFeatures/envVars/InjectAwsCredsToEnvVars.java
@@ -8,6 +8,7 @@ import jetbrains.buildServer.agent.Constants;
 import jetbrains.buildServer.clouds.amazon.connector.AwsConnectorFactory;
 import jetbrains.buildServer.clouds.amazon.connector.utils.parameters.AwsCloudConnectorConstants;
 import jetbrains.buildServer.clouds.amazon.connector.utils.parameters.AwsConnBuildFeatureParams;
+import jetbrains.buildServer.log.Loggers;
 import jetbrains.buildServer.serverSide.*;
 import jetbrains.buildServer.serverSide.oauth.OAuthConnectionDescriptor;
 import jetbrains.buildServer.serverSide.oauth.OAuthConnectionsManager;
@@ -31,6 +32,10 @@ public class InjectAwsCredsToEnvVars implements BuildStartContextProcessor, Pass
   public void updateParameters(@NotNull BuildStartContext context) {
     //TODO: add support for several AWS Connections exposing
     ArrayList<OAuthConnectionDescriptor> awsConnections = AwsConnToEnvVarsBuildFeature.getAwsConnections(context.getBuild(), myOAuthConnectionsManager);
+    if(awsConnections.size() == 0){
+      Loggers.CLOUD.warn("There is no AWS Connection to expose. Check that chosen AWS Connection exists.");
+      return;
+    }
 
     context.addSharedParameter(Constants.ENV_PREFIX + AwsConnBuildFeatureParams.AWS_REGION_ENV_PARAM_DEFAULT, emptyIfNull(awsConnections.get(0).getParameters().get(AwsCloudConnectorConstants.REGION_NAME_PARAM)));
 
@@ -49,6 +54,10 @@ public class InjectAwsCredsToEnvVars implements BuildStartContextProcessor, Pass
   public Collection<Parameter> getPasswordParameters(@NotNull SBuild build) {
     ArrayList<Parameter> secureParams = new ArrayList<>();
     ArrayList<OAuthConnectionDescriptor> awsConnections = AwsConnToEnvVarsBuildFeature.getAwsConnections(build, myOAuthConnectionsManager);
+    if(awsConnections.size() == 0){
+      Loggers.CLOUD.warn("There is no AWS Connection to expose. Check that chosen AWS Connection exists.");
+      return new ArrayList<>();
+    }
 
     AWSCredentialsProvider creds = myAwsConnectorFactory.buildAwsCredentialsProvider(awsConnections.get(0).getParameters());
 

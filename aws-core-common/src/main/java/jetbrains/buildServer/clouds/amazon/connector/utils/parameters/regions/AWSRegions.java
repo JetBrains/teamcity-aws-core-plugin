79e3601fed1b72d68016eafff06c5f37104e4c38
andrey.yuldashev <andrey.yuldashev@jetbrains.com>
2022-04-10 20:57:19 +0300

    Change Dublin to Ireland.

--- aws-core-common/src/main/java/jetbrains/buildServer/clouds/amazon/connector/utils/parameters/regions/AWSRegions.java
+++ aws-core-common/src/main/java/jetbrains/buildServer/clouds/amazon/connector/utils/parameters/regions/AWSRegions.java
@@ -36,7 +36,7 @@ public final class AWSRegions {
     REGION_NAMES_FOR_WEB.put("us-west-2", "US West (Oregon)");
     REGION_NAMES_FOR_WEB.put("ca-central-1", "Canada (Central)");
     REGION_NAMES_FOR_WEB.put("eu-north-1", "EU North (Stockholm)");
-    REGION_NAMES_FOR_WEB.put("eu-west-1", "EU West (Dublin)");
+    REGION_NAMES_FOR_WEB.put("eu-west-1", "EU West (Ireland)");
     REGION_NAMES_FOR_WEB.put("eu-west-2", "EU West (London)");
     REGION_NAMES_FOR_WEB.put("eu-west-3", "EU West (Paris)");
     REGION_NAMES_FOR_WEB.put("eu-central-1", "EU Central (Frankfurt)");

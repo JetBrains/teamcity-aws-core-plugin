cd9833e8cc550fe482c1840892ed0cf72a08e1b1
andrey.yuldashev <andrey.yuldashev@jetbrains.com>
2022-05-30 15:28:28 +0300

    Add test for the AwsRotateKeysController.

--- aws-core-server/src/test/java/jetbrains/buildServer/serverSide/oauth/aws/controllers/AwsRotateKeysControllerTest.java
+++ aws-core-server/src/test/java/jetbrains/buildServer/serverSide/oauth/aws/controllers/AwsRotateKeysControllerTest.java
@@ -16,182 +16,261 @@
 
 package jetbrains.buildServer.serverSide.oauth.aws.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.amazonaws.services.identitymanagement.AmazonIdentityManagement;
+import com.amazonaws.services.identitymanagement.model.*;
+import com.amazonaws.services.securitytoken.AWSSecurityTokenService;
+import com.amazonaws.services.securitytoken.model.GetCallerIdentityResult;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jetbrains.buildServer.clouds.amazon.connector.errors.AwsConnectorException;
-import jetbrains.buildServer.clouds.amazon.connector.errors.KeyRotationException;
 import jetbrains.buildServer.clouds.amazon.connector.keyRotation.AwsKeyRotator;
-import jetbrains.buildServer.clouds.amazon.connector.utils.parameters.AwsAccessKeysParams;
-import jetbrains.buildServer.clouds.amazon.connector.utils.parameters.AwsCloudConnectorConstants;
+import jetbrains.buildServer.clouds.amazon.connector.keyRotation.impl.AwsKeyRotatorImpl;
 import jetbrains.buildServer.controllers.ActionErrors;
-import jetbrains.buildServer.controllers.AuthorizationInterceptor;
-import jetbrains.buildServer.serverSide.SBuildServer;
-import jetbrains.buildServer.serverSide.SProject;
+import jetbrains.buildServer.controllers.BaseControllerTestCase;
+import jetbrains.buildServer.serverSide.impl.ProjectFeatureDescriptorImpl;
 import jetbrains.buildServer.serverSide.oauth.OAuthConnectionDescriptor;
 import jetbrains.buildServer.serverSide.oauth.OAuthConnectionsManager;
-import jetbrains.buildServer.testUtils.AbstractControllerTest;
-import jetbrains.buildServer.web.openapi.WebControllerManager;
+import jetbrains.buildServer.serverSide.oauth.OAuthConstants;
+import jetbrains.buildServer.serverSide.oauth.aws.AwsConnectionProvider;
 import org.mockito.Mockito;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 import static jetbrains.buildServer.clouds.amazon.connector.utils.parameters.AwsAccessKeysParams.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
-public class AwsRotateKeysControllerTest extends AbstractControllerTest {
+public class AwsRotateKeysControllerTest extends BaseControllerTestCase<AwsRotateKeysController> {
+
+  private final String TEST_CONN_FEATURE_ID = "EXT_1";
+
+  private final String CURRENT_ACCESS_KEY = "EXAMPLE_ACCESS";
+  private final String CURRENT_SECRET_KEY = "EXAMPLE_SECRET";
+  private final String ROTATED_ACCESS_KEY = "ROTATED_ACCESS";
+  private final String ROTATED_SECRET_KEY = "ROTATED_SECRET";
+
+  private final String TEST_USER_NAME = "SomeUser";
 
   private final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-  private final String AWS_CONNECTION_ID = "PROJECT_FEATURE_ID";
-  private final String AWS_ACCESS_KEY_ID = "ACCESSKEY";
-  private final String AWS_SECRET_ACCESS_KEY = "SECRETKEY";
-  private final String ROTATED_AWS_ACCESS_KEY_ID = "ACCESSKEYROTATED";
-  private final String ROTATED_AWS_SECRET_ACCESS_KEY = "SECRETKEYROTATED";
-  private AwsRotateKeysController myAwsRotateKeysController;
-  private OAuthConnectionsManager connectionsManager;
-  private OAuthConnectionDescriptor mockedAwsConnection;
 
+  private OAuthConnectionsManager myOAuthConnectionsManager;
+  private AmazonIdentityManagement iam;
+  private AWSSecurityTokenService sts;
+
+  @Override
   @BeforeMethod
-  public void setUp() throws IOException {
+  public void setUp() throws Exception {
     super.setUp();
 
-    setInternalProperty(AwsCloudConnectorConstants.FEATURE_PROPERTY_NAME, "true");
+    myProject = myFixture.createProject("AWS Connection Key Rotation Test Project");
+    addTestConnection();
+  }
 
-    SProject project = Mockito.mock(SProject.class);
-    when(projectManager.findProjectByExternalId(PROJECT_ID))
-      .thenReturn(project);
+  private void addTestConnection() {
+    Map<String, String> projectFeatureProperties = new HashMap<>();
+    projectFeatureProperties.put(OAuthConstants.OAUTH_TYPE_PARAM, AwsConnectionProvider.TYPE);
+    projectFeatureProperties.put(ACCESS_KEY_ID_PARAM, CURRENT_ACCESS_KEY);
+    projectFeatureProperties.put(SECURE_SECRET_ACCESS_KEY_PARAM, CURRENT_SECRET_KEY);
 
-    mockedAwsConnection = Mockito.mock(OAuthConnectionDescriptor.class);
-    when(mockedAwsConnection.getId())
-      .thenReturn(AWS_CONNECTION_ID);
-    when(mockedAwsConnection.getParameters())
-      .thenReturn(createDefaultProperties());
+    ProjectFeatureDescriptorImpl projectFeature = new ProjectFeatureDescriptorImpl(
+      TEST_CONN_FEATURE_ID,
+      OAuthConstants.FEATURE_TYPE,
+      projectFeatureProperties,
+      myProject.getProjectId()
+    );
+    myProject.addFeature(projectFeature);
+  }
 
-    connectionsManager = Mockito.mock(OAuthConnectionsManager.class);
-    when(connectionsManager.findConnectionById(project, mockedAwsConnection.getId()))
-      .thenReturn(mockedAwsConnection);
-
-    myAwsRotateKeysController = new AwsRotateKeysController(
-      Mockito.mock(SBuildServer.class),
-      Mockito.mock(WebControllerManager.class),
-      projectManager,
-      Mockito.spy(AuthorizationInterceptor.class),
-      createRotator()
+  @Override
+  protected AwsRotateKeysController createController() {
+    iam = Mockito.mock(AmazonIdentityManagement.class);
+    sts = Mockito.mock(AWSSecurityTokenService.class);
+    myOAuthConnectionsManager = new OAuthConnectionsManager(myServer);
+    return new AwsRotateKeysController(
+      myServer,
+      myWebManager,
+      myProjectManager,
+      getWebFixture().getAuthorizationInterceptor(),
+      createKeyRotator()
     );
   }
 
-  private Map<String, String> createDefaultProperties() {
-    Map<String, String> res = new HashMap<>();
-    res.put(AwsAccessKeysParams.ACCESS_KEY_ID_PARAM, AWS_ACCESS_KEY_ID);
-    res.put(AwsAccessKeysParams.SECURE_SECRET_ACCESS_KEY_PARAM, AWS_SECRET_ACCESS_KEY);
-    res.put(AwsCloudConnectorConstants.CREDENTIALS_TYPE_PARAM, AwsCloudConnectorConstants.STATIC_CREDENTIALS_TYPE);
-    res.put(AwsCloudConnectorConstants.REGION_NAME_PARAM, AwsCloudConnectorConstants.REGION_NAME_DEFAULT);
-    return res;
-  }
+  private AwsKeyRotator createKeyRotator() {
 
-  private AwsKeyRotator createRotator() {
-    return (connectionId, project) -> {
-      OAuthConnectionDescriptor connection = connectionsManager.findConnectionById(project, connectionId);
-      if (connection == null) {
-        throw new KeyRotationException("Connection to rotate was not found, ID is: " + connectionId);
-      }
+    when(iam.getUser(any()))
+      .thenReturn(new GetUserResult()
+        .withUser(new User()
+          .withUserName(TEST_USER_NAME)));
 
-      Map<String, String> rotatedProperties = createDefaultProperties();
-      rotatedProperties.put(ACCESS_KEY_ID_PARAM, ROTATED_AWS_ACCESS_KEY_ID);
-      rotatedProperties.put(SECURE_SECRET_ACCESS_KEY_PARAM, ROTATED_AWS_SECRET_ACCESS_KEY);
+    when(iam.createAccessKey(any()))
+      .thenReturn(new CreateAccessKeyResult()
+        .withAccessKey(new AccessKey()
+          .withAccessKeyId(ROTATED_ACCESS_KEY)
+          .withSecretAccessKey(ROTATED_SECRET_KEY)));
 
-      when(connection.getParameters())
-        .thenReturn(rotatedProperties);
-    };
-  }
+    when(sts.getCallerIdentity(any()))
+      .thenReturn(
+        new GetCallerIdentityResult());
 
-  @Test
-  public void givenAllParams_withRotateKeyRequest_thenReturnRotatedKey() throws JsonProcessingException {
-    when(request.getParameter("connectionId"))
-      .thenReturn(AWS_CONNECTION_ID);
-
-    try {
-      myAwsRotateKeysController.doHandle(request, response);
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail();
-    }
-
-    String result = responseOutputStream.toString();
-    ActionErrors expectedErrors = new ActionErrors();
-    String expectedErrorsJson = OBJECT_MAPPER.writeValueAsString(expectedErrors);
-
-    assertEquals(expectedErrorsJson, result);
-    assertEquals(ROTATED_AWS_ACCESS_KEY_ID, mockedAwsConnection.getParameters().get(ACCESS_KEY_ID_PARAM));
-    assertEquals(ROTATED_AWS_SECRET_ACCESS_KEY, mockedAwsConnection.getParameters().get(SECURE_SECRET_ACCESS_KEY_PARAM));
+    return new AwsKeyRotatorImpl(
+      myOAuthConnectionsManager,
+      myFixture.getEventDispatcher(),
+      iam,
+      sts
+    );
   }
 
   @Test
-  public void givenAllParams_withNullProjectId_thenReturnError() throws JsonProcessingException {
-    when(request.getParameter("projectId"))
-      .thenReturn(null);
+  public void givenProjectAndConnection_whenRotationRequested_thenReturnRotatedConnection() throws Exception {
 
-    try {
-      myAwsRotateKeysController.doHandle(request, response);
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail();
-    }
+    doPost("projectId", myProject.getExternalId(),
+      "connectionId", TEST_CONN_FEATURE_ID);
 
-    String result = responseOutputStream.toString();
     ActionErrors expectedErrors = new ActionErrors();
-    expectedErrors.addError(ROTATE_KEY_BTTN_ID, "Unable to rotate keys: The ID of the project where to rotate keys is null.");
-    String expectedErrorsJson = OBJECT_MAPPER.writeValueAsString(expectedErrors);
+    assertEquals(OBJECT_MAPPER.writeValueAsString(expectedErrors), myResponse.getReturnedContent());
 
-    assertEquals(expectedErrorsJson, result);
-    assertEquals(AWS_ACCESS_KEY_ID, mockedAwsConnection.getParameters().get(ACCESS_KEY_ID_PARAM));
-    assertEquals(AWS_SECRET_ACCESS_KEY, mockedAwsConnection.getParameters().get(SECURE_SECRET_ACCESS_KEY_PARAM));
+    OAuthConnectionDescriptor connection = myOAuthConnectionsManager.findConnectionById(myProject, TEST_CONN_FEATURE_ID);
+    if (connection == null)
+      fail("Rotated connection was not found");
+
+    assertEquals(
+      ROTATED_ACCESS_KEY,
+      connection
+        .getParameters()
+        .get(ACCESS_KEY_ID_PARAM)
+    );
+
+    assertEquals(
+      ROTATED_SECRET_KEY,
+      connection
+        .getParameters()
+        .get(SECURE_SECRET_ACCESS_KEY_PARAM)
+    );
   }
 
   @Test
-  public void givenAllParams_withNonExistingProject_thenReturnError() throws JsonProcessingException {
-    when(request.getParameter("projectId"))
-      .thenReturn("NONEXISTING");
+  public void givenProjectAndNonExistingConnection_whenRotationRequested_thenReturnError() throws Exception {
 
-    try {
-      myAwsRotateKeysController.doHandle(request, response);
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail();
-    }
+    doPost("projectId", myProject.getExternalId(),
+      "connectionId", "NON_EXISING");
 
-    String result = responseOutputStream.toString();
     ActionErrors expectedErrors = new ActionErrors();
-    expectedErrors.addError(ROTATE_KEY_BTTN_ID, "Unable to rotate keys: Could not find the project with id: NONEXISTING");
-    String expectedErrorsJson = OBJECT_MAPPER.writeValueAsString(expectedErrors);
+    expectedErrors.addError(ROTATE_KEY_BTTN_ID, "Unable to rotate keys: The AWS Connection with ID NON_EXISING was not found.");
+    assertEquals(OBJECT_MAPPER.writeValueAsString(expectedErrors), myResponse.getReturnedContent());
 
-    assertEquals(expectedErrorsJson, result);
-    assertEquals(AWS_ACCESS_KEY_ID, mockedAwsConnection.getParameters().get(ACCESS_KEY_ID_PARAM));
-    assertEquals(AWS_SECRET_ACCESS_KEY, mockedAwsConnection.getParameters().get(SECURE_SECRET_ACCESS_KEY_PARAM));
+    OAuthConnectionDescriptor connection = myOAuthConnectionsManager.findConnectionById(myProject, TEST_CONN_FEATURE_ID);
+    if (connection == null)
+      fail("Rotated connection was not found");
+
+    assertEquals(
+      CURRENT_ACCESS_KEY,
+      connection
+        .getParameters()
+        .get(ACCESS_KEY_ID_PARAM)
+    );
+
+    assertEquals(
+      CURRENT_SECRET_KEY,
+      connection
+        .getParameters()
+        .get(SECURE_SECRET_ACCESS_KEY_PARAM)
+    );
   }
 
   @Test
-  public void givenAllParams_withNonExistingConnection_thenReturnError() throws JsonProcessingException {
-    when(request.getParameter("connectionId"))
-      .thenReturn("NONEXISTING");
+  public void givenProjectAndConnection_whenAwsRotateRequestFailed_thenReturnError() throws Exception {
 
-    try {
-      myAwsRotateKeysController.doHandle(request, response);
-    } catch (Exception e) {
-      e.printStackTrace();
-      fail();
-    }
+    when(iam.createAccessKey(any()))
+      .thenThrow(new LimitExceededException("There are 2 access key already."));
 
-    String result = responseOutputStream.toString();
+    doPost("projectId", myProject.getExternalId(),
+      "connectionId", TEST_CONN_FEATURE_ID);
+
     ActionErrors expectedErrors = new ActionErrors();
-    expectedErrors.addError(ROTATE_KEY_BTTN_ID, "Unable to rotate keys: Connection to rotate was not found, ID is: NONEXISTING");
-    String expectedErrorsJson = OBJECT_MAPPER.writeValueAsString(expectedErrors);
+    expectedErrors.addError(ROTATE_KEY_BTTN_ID, "Unable to rotate keys: There are 2 access key already.");
+    assertEquals(OBJECT_MAPPER.writeValueAsString(expectedErrors), myResponse.getReturnedContent());
 
-    assertEquals(expectedErrorsJson, result);
-    assertEquals(AWS_ACCESS_KEY_ID, mockedAwsConnection.getParameters().get(ACCESS_KEY_ID_PARAM));
-    assertEquals(AWS_SECRET_ACCESS_KEY, mockedAwsConnection.getParameters().get(SECURE_SECRET_ACCESS_KEY_PARAM));
+    OAuthConnectionDescriptor connection = myOAuthConnectionsManager.findConnectionById(myProject, TEST_CONN_FEATURE_ID);
+    if (connection == null)
+      fail("Rotated connection was not found");
+
+    assertEquals(
+      CURRENT_ACCESS_KEY,
+      connection
+        .getParameters()
+        .get(ACCESS_KEY_ID_PARAM)
+    );
+
+    assertEquals(
+      CURRENT_SECRET_KEY,
+      connection
+        .getParameters()
+        .get(SECURE_SECRET_ACCESS_KEY_PARAM)
+    );
+  }
+
+  @Test
+  public void givenProjectAndConnection_whenAwsRotateRequestTimedOut_thenReturnError() throws Exception {
+
+    when(sts.getCallerIdentity(any()))
+      .thenThrow(new RuntimeException("Dummy timeout."));
+
+    doPost("projectId", myProject.getExternalId(),
+      "connectionId", TEST_CONN_FEATURE_ID);
+
+    ActionErrors expectedErrors = new ActionErrors();
+    expectedErrors.addError(ROTATE_KEY_BTTN_ID, "Unable to rotate keys: Rotated connection is invalid after 1 seconds: Dummy timeout.");
+    assertEquals(OBJECT_MAPPER.writeValueAsString(expectedErrors), myResponse.getReturnedContent());
+
+    OAuthConnectionDescriptor connection = myOAuthConnectionsManager.findConnectionById(myProject, TEST_CONN_FEATURE_ID);
+    if (connection == null)
+      fail("Rotated connection was not found");
+
+    assertEquals(
+      CURRENT_ACCESS_KEY,
+      connection
+        .getParameters()
+        .get(ACCESS_KEY_ID_PARAM)
+    );
+
+    assertEquals(
+      CURRENT_SECRET_KEY,
+      connection
+        .getParameters()
+        .get(SECURE_SECRET_ACCESS_KEY_PARAM)
+    );
+  }
+
+  @Test
+  public void givenProjectWithManyConnections_whenAwsRotateAllKeysRequested_thenReturnRotatedConnections() throws Exception {
+
+    when(sts.getCallerIdentity(any()))
+      .thenThrow(new RuntimeException("Dummy timeout."));
+
+    doPost("projectId", myProject.getExternalId(),
+      "connectionId", TEST_CONN_FEATURE_ID);
+
+    ActionErrors expectedErrors = new ActionErrors();
+    expectedErrors.addError(ROTATE_KEY_BTTN_ID, "Unable to rotate keys: Rotated connection is invalid after 1 seconds: Dummy timeout.");
+    assertEquals(OBJECT_MAPPER.writeValueAsString(expectedErrors), myResponse.getReturnedContent());
+
+    OAuthConnectionDescriptor connection = myOAuthConnectionsManager.findConnectionById(myProject, TEST_CONN_FEATURE_ID);
+    if (connection == null)
+      fail("Rotated connection was not found");
+
+    assertEquals(
+      CURRENT_ACCESS_KEY,
+      connection
+        .getParameters()
+        .get(ACCESS_KEY_ID_PARAM)
+    );
+
+    assertEquals(
+      CURRENT_SECRET_KEY,
+      connection
+        .getParameters()
+        .get(SECURE_SECRET_ACCESS_KEY_PARAM)
+    );
   }
 }

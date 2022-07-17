422f17ad8e9b86624218308723b809a54774779e
andrey.yuldashev <andrey.yuldashev@jetbrains.com>
2022-07-17 11:34:35 +0300

    Add check to avoid concurrent task execution on multiple nodes.

--- aws-core-server/src/main/java/jetbrains/buildServer/clouds/amazon/connector/keyRotation/impl/OldKeysCleaner.java
+++ aws-core-server/src/main/java/jetbrains/buildServer/clouds/amazon/connector/keyRotation/impl/OldKeysCleaner.java
@@ -6,6 +6,7 @@ import jetbrains.buildServer.clouds.amazon.connector.utils.parameters.ParamUtil;
 import jetbrains.buildServer.log.Loggers;
 import jetbrains.buildServer.serverSide.CannotAcceptTaskException;
 import jetbrains.buildServer.serverSide.MultiNodeTasks;
+import jetbrains.buildServer.serverSide.ServerResponsibility;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.ZoneId;
@@ -18,19 +19,24 @@ public class OldKeysCleaner {
   private static final String DELETE_OLD_AWS_KEY_TASK_TYPE = "deleteOldAwsKey";
 
   private final MultiNodeTasks myMultiNodeTasks;
+  private final ServerResponsibility myServerResponsibility;
   private final TemporalAmount myOldKeyPreserveTime;
 
   private final ConcurrentHashMap<String, RotateKeyApi> oldKeysRotateApis = new ConcurrentHashMap<>();
 
   public OldKeysCleaner(@NotNull MultiNodeTasks multiNodeTasks,
+                        @NotNull final ServerResponsibility serverResponsibility,
                         @NotNull final TemporalAmount oldKeyPreserveTime) {
     myMultiNodeTasks = multiNodeTasks;
+    myServerResponsibility = serverResponsibility;
     myOldKeyPreserveTime = oldKeyPreserveTime;
 
     myMultiNodeTasks.subscribe(DELETE_OLD_AWS_KEY_TASK_TYPE, new MultiNodeTasks.TaskConsumer() {
       @Override
       public boolean beforeAccept(@NotNull final MultiNodeTasks.PerformingTask task) {
-        if (!DELETE_OLD_AWS_KEY_TASK_TYPE.equals(task.getType()) || task.getStringArg() == null) {
+        if (! myServerResponsibility.canWriteToConfigDirectory() ||
+            ! DELETE_OLD_AWS_KEY_TASK_TYPE.equals(task.getType()) ||
+            task.getStringArg() == null) {
           return false;
         }
 

c256b152d84438e0f9e02671eda2b42589d78255
andrey.yuldashev <andrey.yuldashev@jetbrains.com>
2022-08-13 10:47:59 +0300

    Dont use the capital letter.

--- aws-core-common/src/main/java/jetbrains/buildServer/clouds/amazon/connector/utils/parameters/AwsCloudConnectorConstants.java
+++ aws-core-common/src/main/java/jetbrains/buildServer/clouds/amazon/connector/utils/parameters/AwsCloudConnectorConstants.java
@@ -13,7 +13,7 @@ public final class AwsCloudConnectorConstants {
   public static final String CREDENTIALS_TYPE_LABEL = "Type";
 
   public static final String STATIC_CREDENTIALS_TYPE = "awsAccessKeys";
-  public static final String ACCESS_KEYS_LABEL = "Access Keys";
+  public static final String ACCESS_KEYS_LABEL = "Access keys";
 
   public static final String IAM_ROLE_CREDENTIALS_TYPE = "awsAssumeIamRole";
 

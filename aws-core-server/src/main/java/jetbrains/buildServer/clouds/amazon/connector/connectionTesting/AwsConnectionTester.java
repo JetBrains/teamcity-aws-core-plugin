bf8c133590c1d20f2c972413bd11dac5266703b2
andrey.yuldashev <andrey.yuldashev@jetbrains.com>
2022-09-28 14:44:57 +0300

    Fix Connection testing when using IAM Role External ID
    When connection is tested only its properties were tested, but when External ID is being used on the AWS side we need to test connection with project and connection IDs in order to set the right IAM Role External ID

--- aws-core-server/src/main/java/jetbrains/buildServer/clouds/amazon/connector/connectionTesting/AwsConnectionTester.java
+++ aws-core-server/src/main/java/jetbrains/buildServer/clouds/amazon/connector/connectionTesting/AwsConnectionTester.java
@@ -5,11 +5,12 @@ import java.util.Map;
 import jetbrains.buildServer.clouds.amazon.connector.connectionTesting.impl.AwsTestConnectionResult;
 import jetbrains.buildServer.clouds.amazon.connector.errors.AwsConnectorException;
 import jetbrains.buildServer.serverSide.InvalidProperty;
+import jetbrains.buildServer.serverSide.impl.ProjectFeatureDescriptorImpl;
 import org.jetbrains.annotations.NotNull;
 
 public interface AwsConnectionTester {
   @NotNull
-  AwsTestConnectionResult testConnection(@NotNull final Map<String, String> connectionProperties) throws AwsConnectorException;
+  AwsTestConnectionResult testConnection(@NotNull final ProjectFeatureDescriptorImpl connectionFeature) throws AwsConnectorException;
 
   @NotNull
   List<InvalidProperty> getInvalidProperties(@NotNull final Map<String, String> connectionProperties);

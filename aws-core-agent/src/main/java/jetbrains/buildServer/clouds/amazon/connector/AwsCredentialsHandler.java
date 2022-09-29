cde7b5fd197debaef224f631ab3db354a329cc3d
Andrei Efimov <andrei.efimov@jetbrains.com>
2022-09-29 11:57:31 +0200

    fix: content saved between stages
    For some reason both tmp directories buildTmp and agentTmp are cleaned right after buildStarted event. We can't inject our listener right after it still staying in the buildStarted phase. If we process injected content outside of the buildStarted phase, injected parameter will leak.

--- aws-core-agent/src/main/java/jetbrains/buildServer/clouds/amazon/connector/AwsCredentialsHandler.java
+++ aws-core-agent/src/main/java/jetbrains/buildServer/clouds/amazon/connector/AwsCredentialsHandler.java
@@ -5,10 +5,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Base64;
-import jetbrains.buildServer.agent.AgentBuildSettings;
-import jetbrains.buildServer.agent.AgentLifeCycleAdapter;
-import jetbrains.buildServer.agent.AgentLifeCycleListener;
-import jetbrains.buildServer.agent.AgentRunningBuild;
+import jetbrains.buildServer.agent.*;
 import jetbrains.buildServer.clouds.amazon.connector.utils.parameters.AwsConnBuildFeatureParams;
 import jetbrains.buildServer.messages.DefaultMessagesInfo;
 import jetbrains.buildServer.util.EventDispatcher;
@@ -20,6 +17,7 @@ public class AwsCredentialsHandler extends AgentLifeCycleAdapter {
   private static final Logger LOG = Logger.getInstance(AwsCredentialsHandler.class);
   public static final String AWS_CREDENTIALS_FILE_NAME = "aws.credentials";
   public static final String EMPTY_STRING = "";
+  private byte[] myCredentialsData;
 
   public AwsCredentialsHandler(@NotNull EventDispatcher<AgentLifeCycleListener> agentDispatcher) {
     agentDispatcher.addListener(this);
@@ -38,28 +36,46 @@ public class AwsCredentialsHandler extends AgentLifeCycleAdapter {
     }
 
     try {
-      final byte[] credentialsData = Base64.getDecoder().decode(encodedCredentials);
-      final File awsCredentialsFile = createFileInTempDirectory(runningBuild);
-
-      try (FileOutputStream os = new FileOutputStream(awsCredentialsFile)) {
-        os.write(credentialsData);
-      }
-
-      runningBuild.getBuildLogger().logMessage(DefaultMessagesInfo.createTextMessage("AWS credentials are successfully associated with this build"));
-
-      runningBuild.addSharedEnvironmentVariable(
-        AwsConnBuildFeatureParams.AWS_SHARED_CREDENTIALS_FILE,
-        FileUtil.getCanonicalFile(awsCredentialsFile).getAbsolutePath()
-      );
-
+      myCredentialsData = Base64.getDecoder().decode(encodedCredentials);
       runningBuild.getBuildLogger().logMessage(DefaultMessagesInfo.createTextMessage("Using AWS Access key: " + awsAccessKey));
+      runningBuild.getBuildLogger().logMessage(DefaultMessagesInfo.createTextMessage("AWS credentials are successfully associated with this build"));
     } catch (Exception e) {
-      String msg = "Failed to create temporary file for AWS credentials. Error: " + e.getMessage();
+      String msg = "Parsing of AWS credentials failed. Error: " + e.getMessage();
       LOG.warn(msg, e);
       runningBuild.getBuildLogger().warning(msg);
     } finally {
       runningBuild.addSharedConfigParameter(AwsConnBuildFeatureParams.AWS_INTERNAL_ENCODED_CREDENTIALS_CONTENT, EMPTY_STRING);
     }
+  }
+
+  @Override
+  public void beforeRunnerStart(@NotNull BuildRunnerContext runner) {
+    if (myCredentialsData == null) {
+      return;
+    }
+
+    try {
+      final File awsCredentialsFile = createFileInTempDirectory(runner.getBuild());
+
+      try (FileOutputStream os = new FileOutputStream(awsCredentialsFile)) {
+        os.write(myCredentialsData);
+      }
+
+      runner.addEnvironmentVariable(
+        AwsConnBuildFeatureParams.AWS_SHARED_CREDENTIALS_FILE,
+        awsCredentialsFile.getAbsolutePath()
+      );
+
+    } catch (Exception e) {
+      String msg = "Failed to create temporary file for AWS credentials. Error: " + e.getMessage();
+      LOG.warn(msg, e);
+      runner.getBuild().getBuildLogger().warning(msg);
+    }
+  }
+
+  @Override
+  public void buildFinished(@NotNull AgentRunningBuild build, @NotNull BuildFinishedStatus buildStatus) {
+    myCredentialsData = null;
   }
 
   private File createFileInTempDirectory(@NotNull AgentBuildSettings build) throws IOException {

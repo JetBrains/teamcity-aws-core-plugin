d377c4a2b59967a36c51feef09ab7bfedd480d47
andrey.yuldashev <andrey.yuldashev@jetbrains.com>
2023-01-15 15:50:33 +0200

    TW-79312 AWS Connection: Add an easy way to allow all AWS STS endpoints via internal property

--- aws-core-common/src/main/java/jetbrains/buildServer/clouds/amazon/connector/utils/parameters/StsEndpointParamValidator.java
+++ aws-core-common/src/main/java/jetbrains/buildServer/clouds/amazon/connector/utils/parameters/StsEndpointParamValidator.java
@@ -19,6 +19,8 @@ public class StsEndpointParamValidator {
   public static final String GLOBAL_AWS_STS_ENDPOINT = "https://sts.amazonaws.com";
   public static final String STS_ENDPOINTS_ALLOWLIST_SEPARATOR = ",";
 
+  public static final String STS_ENDPOINTS_ALLOWLIST_ALL_AWS_ENDPOINTS_EXPRESSION = "[aws]";
+
   public static boolean isValidStsEndpoint(@Nullable final String url) {
     if (url == null) {
       return false;
@@ -31,19 +33,30 @@ public class StsEndpointParamValidator {
     List<String> res = new ArrayList<>();
     String allowedStsEndpointsProperty = TeamCityProperties.getPropertyOrNull(STS_ENDPOINTS_ALLOWLIST_PROPERTY_NAME);
 
-    if (StringUtil.nullIfEmpty(allowedStsEndpointsProperty) != null) {
-      res = parseAllowedStsEndpoints(allowedStsEndpointsProperty);
-    } else {
-      for (String regionName : AWSRegions.getAllRegions().keySet()) {
-        if (isChinaRegion(regionName)) {
-          res.add(String.format(CHINA_REGION_TO_STS_ENDPOINT_FORMAT, regionName));
-        } else {
-          res.add(String.format(REGION_TO_STS_ENDPOINT_FORMAT, regionName));
-        }
-      }
-      res.add(GLOBAL_AWS_STS_ENDPOINT);
+    if (StringUtil.nullIfEmpty(allowedStsEndpointsProperty) == null) {
+      addAllDefaultAwsStsEndpoints(res);
+      return res;
     }
+
+    res = parseAllowedStsEndpoints(allowedStsEndpointsProperty);
+
+    if (res.contains(STS_ENDPOINTS_ALLOWLIST_ALL_AWS_ENDPOINTS_EXPRESSION)) {
+      addAllDefaultAwsStsEndpoints(res);
+      res.remove(STS_ENDPOINTS_ALLOWLIST_ALL_AWS_ENDPOINTS_EXPRESSION);
+    }
+
     return res;
+  }
+
+  private static void addAllDefaultAwsStsEndpoints(@NotNull List<String> res) {
+    for (String regionName : AWSRegions.getAllRegions().keySet()) {
+      if (isChinaRegion(regionName)) {
+        res.add(String.format(CHINA_REGION_TO_STS_ENDPOINT_FORMAT, regionName));
+      } else {
+        res.add(String.format(REGION_TO_STS_ENDPOINT_FORMAT, regionName));
+      }
+    }
+    res.add(GLOBAL_AWS_STS_ENDPOINT);
   }
 
   private static List<String> parseAllowedStsEndpoints(@NotNull final String allowedStsEndpointsString) {
--- aws-core-common/src/test/java/jetbrains/buildServer/clouds/amazon/connector/utils/parameters/StsEndpointParamValidatorTest.java
+++ aws-core-common/src/test/java/jetbrains/buildServer/clouds/amazon/connector/utils/parameters/StsEndpointParamValidatorTest.java
@@ -1,17 +1,33 @@
 package jetbrains.buildServer.clouds.amazon.connector.utils.parameters;
 
+import jetbrains.buildServer.BaseTestCase;
 import org.testng.annotations.Test;
 
+import static jetbrains.buildServer.clouds.amazon.connector.utils.parameters.StsEndpointParamValidator.STS_ENDPOINTS_ALLOWLIST_ALL_AWS_ENDPOINTS_EXPRESSION;
+import static jetbrains.buildServer.clouds.amazon.connector.utils.parameters.StsEndpointParamValidator.STS_ENDPOINTS_ALLOWLIST_PROPERTY_NAME;
 import static org.testng.Assert.*;
 
-public class StsEndpointParamValidatorTest {
+public class StsEndpointParamValidatorTest extends BaseTestCase {
   private final String CHINA_ENDPOINT_URL_1= "https://sts.cn-north-1.amazonaws.com.cn";
   private final String CHINA_ENDPOINT_URL_2 = "https://sts.cn-northwest-1.amazonaws.com.cn";
 
 
   @Test
   public void when_china_endpoint_then_accept_without_errors() {
+    removeInternalProperty(STS_ENDPOINTS_ALLOWLIST_PROPERTY_NAME);
+
     assertTrue(StsEndpointParamValidator.isValidStsEndpoint(CHINA_ENDPOINT_URL_1));
     assertTrue(StsEndpointParamValidator.isValidStsEndpoint(CHINA_ENDPOINT_URL_2));
+  }
+
+  @Test
+  public void when_with_all_aws_endpoints_expr_plus_custom_then_accept_all_aws_endpoints_and_custom() {
+    String customEndpoint = "http://some.custom.com";
+    String internalPropValue = STS_ENDPOINTS_ALLOWLIST_ALL_AWS_ENDPOINTS_EXPRESSION + "," + customEndpoint;
+    setInternalProperty(STS_ENDPOINTS_ALLOWLIST_PROPERTY_NAME, internalPropValue);
+
+    assertTrue(StsEndpointParamValidator.isValidStsEndpoint(CHINA_ENDPOINT_URL_1));
+    assertTrue(StsEndpointParamValidator.isValidStsEndpoint(CHINA_ENDPOINT_URL_2));
+    assertTrue(StsEndpointParamValidator.isValidStsEndpoint(customEndpoint));
   }
 }

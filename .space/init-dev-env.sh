881b708c95b8d82168076b0a90a50cc32a888d8f
andrey.yuldashev <andrey.yuldashev@jetbrains.com>
2023-06-06 16:48:41 +0300

    Space RD fix 2

--- .space/init-dev-env.sh
+++ .space/init-dev-env.sh
@@ -1,3 +1,4 @@
 mkdir teamcity
 curl --output ./teamcity/TeamCity.tar.gz https://download-cdn.jetbrains.com/teamcity/TeamCity-2023.05.tar.gz
-tar -xvf ./teamcity/TeamCity.tar.gz -C /teamcity/TeamCity
+mkdir teamcity/TeamCity
+tar -xvf ./teamcity/TeamCity.tar.gz -C ./teamcity/TeamCity --strip-components=1

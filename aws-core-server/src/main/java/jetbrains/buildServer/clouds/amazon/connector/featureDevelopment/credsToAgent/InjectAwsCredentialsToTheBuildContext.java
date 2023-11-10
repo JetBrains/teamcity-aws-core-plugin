93d827fe578f9719b9d2099984a21cdb843419af
Andre Rocha <andre.rocha@jetbrains.com>
2023-11-10 16:45:55 +0000

    TW-84251 Ensure available for sub-projects checkbox is respected
    Merge-request: TC-MR-7620
    Merged-by: André Rocha <andre.rocha@jetbrains.com>

--- aws-core-server/src/main/java/jetbrains/buildServer/clouds/amazon/connector/featureDevelopment/credsToAgent/InjectAwsCredentialsToTheBuildContext.java
+++ aws-core-server/src/main/java/jetbrains/buildServer/clouds/amazon/connector/featureDevelopment/credsToAgent/InjectAwsCredentialsToTheBuildContext.java
@@ -1,14 +1,13 @@
 package jetbrains.buildServer.clouds.amazon.connector.featureDevelopment.credsToAgent;
 
 import java.util.List;
+import jetbrains.buildServer.BuildProblemData;
 import jetbrains.buildServer.clouds.amazon.connector.LinkedAwsConnectionProvider;
 import jetbrains.buildServer.clouds.amazon.connector.impl.AwsConnectionCredentials;
 import jetbrains.buildServer.log.Loggers;
-import jetbrains.buildServer.messages.DefaultMessagesInfo;
-import jetbrains.buildServer.messages.Status;
 import jetbrains.buildServer.serverSide.BuildStartContext;
 import jetbrains.buildServer.serverSide.BuildStartContextProcessor;
-import jetbrains.buildServer.serverSide.buildLog.MessageAttrs;
+import jetbrains.buildServer.serverSide.SRunningBuild;
 import jetbrains.buildServer.serverSide.connections.credentials.ConnectionCredentials;
 import jetbrains.buildServer.serverSide.connections.credentials.ConnectionCredentialsException;
 import org.jetbrains.annotations.NotNull;
@@ -16,6 +15,8 @@ import org.jetbrains.annotations.NotNull;
 public class InjectAwsCredentialsToTheBuildContext implements BuildStartContextProcessor {
   private final AwsCredentialsInjector myAwsCredentialsInjector;
   @NotNull private final LinkedAwsConnectionProvider myLinkedAwsConnectionProvider;
+
+  private final String INJECT_CREDENTIALS_PROBLEM_ID = "InjectAwsCredentials";
 
   public InjectAwsCredentialsToTheBuildContext(@NotNull final LinkedAwsConnectionProvider linkedAwsConnectionProvider) {
     myAwsCredentialsInjector = new AwsCredentialsInjector();
@@ -29,8 +30,7 @@ public class InjectAwsCredentialsToTheBuildContext implements BuildStartContextP
       try {
         List<ConnectionCredentials> linkedAwsConnectionCredentials = myLinkedAwsConnectionProvider.getConnectionCredentialsFromBuild(context.getBuild());
         if (linkedAwsConnectionCredentials.isEmpty()) {
-          reportProblem(context, String.format("Build with id: <%s> has AWS Connection to inject, but none was added. Enable debug to see more information.",
-                                               context.getBuild().getBuildId()));
+          finishBuildWithProblem(context, String.format("Build has AWS Connection to inject, but none was added. Enable debug to see more information."));
           return;
         }
 
@@ -42,19 +42,20 @@ public class InjectAwsCredentialsToTheBuildContext implements BuildStartContextP
       } catch (ConnectionCredentialsException e) {
         String warningMessage = "Failed to inject AWS Connection to a build: " + e.getMessage();
         Loggers.CLOUD.warnAndDebugDetails(warningMessage, e);
-        reportProblem(context, warningMessage);
+        finishBuildWithProblem(context, warningMessage);
       }
     }
   }
 
-  private void reportProblem(@NotNull BuildStartContext context, @NotNull String message) {
-    context
-      .getBuild()
-      .getBuildLog()
-      .messageAsync(
-        message,
-        Status.WARNING,
-        MessageAttrs.fromMessage(DefaultMessagesInfo.createTextMessage(message))
-      );
+  private void finishBuildWithProblem(@NotNull BuildStartContext context, @NotNull String message) {
+    final SRunningBuild build = context.getBuild();
+    build.addBuildProblem(
+      BuildProblemData.createBuildProblem(
+        INJECT_CREDENTIALS_PROBLEM_ID,
+        INJECT_CREDENTIALS_PROBLEM_ID,
+        message
+      )
+    );
+    build.stop(null, message);
   }
 }

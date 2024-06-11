36bca703752a0fc6e2d71b79b43ee7ad07cd7c2b
dmitrii.kirkhmeier <dmitrii.kirkhmeier@jetbrains.com>
2024-06-11 14:38:06 +0200

    TW-87868 - fixed documentation link opening in the same tab

--- aws-core-ui/src/App/Components/Type/Default/DefaultConnectionType.tsx
+++ aws-core-ui/src/App/Components/Type/Default/DefaultConnectionType.tsx
@@ -9,6 +9,20 @@ const docUrl = resolveHelpURL('?configuring-connections#AmazonWebServices');
 export default function DefaultConnectionType() {
   const { config } = useApplicationContext();
 
+  const helpOnClick = React.useCallback(
+    (event: React.MouseEvent<HTMLAnchorElement>) => {
+      if (docUrl) {
+        window?.BS?.Util?.showHelp(event, docUrl, {
+          width: 0,
+          height: 0,
+        });
+        event.preventDefault();
+      }
+      return false;
+    },
+    []
+  );
+
   if (config.isDefaultCredProviderEnabled) {
     return <div />;
   } else {
@@ -19,7 +33,9 @@ export default function DefaultConnectionType() {
         {
           ' type is disabled on this server. For instructions on how to enable it and for more information see the '
         }
-        <a href={docUrl}>{'documentation'}</a>
+        <a href={docUrl} onClick={helpOnClick}>
+          {'documentation'}
+        </a>
       </div>
     );
   }

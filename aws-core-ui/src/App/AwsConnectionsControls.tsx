a3ae99dc52aafdd24d6861dddfa14a998725fbd4
Iaroslav Molochkov <iaroslav.molochkov@jetbrains.com>
2024-06-24 13:24:16 +0000

    fix: TW-88164 added the filter to the edit connections button as well
    Merge-request: TC-MR-10748
    Merged-by: Iaroslav Molochkov <iaroslav.molochkov@jetbrains.com>

--- aws-core-ui/src/App/AwsConnectionsControls.tsx
+++ aws-core-ui/src/App/AwsConnectionsControls.tsx
@@ -81,6 +81,7 @@ export default function AwsConnectionsControls({
             setLoading(true);
             getConfigForConnection(connectionData.projectId, currentConnection)
               .then((c) => {
+                c.awsConnectionTypesFilter = connectionData.awsConnectionTypesFilter;
                 c.onClose = () => {
                   deactivateAndReset();
                 };

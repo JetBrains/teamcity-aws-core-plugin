50c79eb1792b550ed997f7036300602f7b687c99
evie.rocha <evie.rocha@jetbrains.com>
2026-01-05 10:29:36 +0000

    TW-97630 Ensure retrier tries to fetch the exceptions in all forms
    Merge-request: TC-MR-19593
    Merged-by: Évie Rocha <Andre.Rocha@jetbrains.com>

--- aws-core-common/src/main/java/jetbrains/buildServer/util/amazon/retry/AmazonRetrier.java
+++ aws-core-common/src/main/java/jetbrains/buildServer/util/amazon/retry/AmazonRetrier.java
@@ -4,10 +4,13 @@ package jetbrains.buildServer.util.amazon.retry;
 
 import com.intellij.openapi.diagnostic.Logger;
 import java.io.IOException;
+import java.io.InterruptedIOException;
 import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
 import javax.net.ssl.SSLException;
+import jetbrains.buildServer.util.ExceptionUtil;
 import jetbrains.buildServer.util.retry.RecoverableException;
 import jetbrains.buildServer.util.retry.Retrier;
 import jetbrains.buildServer.util.retry.RetrierEventListener;
@@ -23,10 +26,10 @@ public interface AmazonRetrier extends RetrierEventListener {
 
   @NotNull
   static Retrier defaultAwsRetrier(final int retriesNum, final int retryDelay, @NotNull final Logger logger) {
-    return Retrier.withRetries(retriesNum, Retrier.DelayStrategy.linearBackOff(retryDelay))
+    return Retrier.withAttempts(retriesNum + 1, Retrier.DelayStrategy.linearBackOff(retryDelay))
                   .registerListener(new LoggingRetrierListener(logger))
                   .registerListener(
-                    new AbortingListener(SSLException.class, UnknownHostException.class, SocketException.class, IOException.class, InterruptedException.class) {
+                    new AbortingListener(ExecutionException.class, SSLException.class, UnknownHostException.class, SocketException.class, InterruptedIOException.class, InterruptedException.class, IOException.class) {
                       @Override
                       public <T> void onFailure(@NotNull Callable<T> callable, int retry, @NotNull Exception e) {
                         if (e instanceof InterruptedException) {
@@ -34,11 +37,13 @@ public interface AmazonRetrier extends RetrierEventListener {
                           return;
                         }
 
-                        if (e instanceof RecoverableException && ((RecoverableException) e).isRecoverable()) {
+                        final RecoverableException recoverableException = ExceptionUtil.getCause(e, RecoverableException.class);
+                        if (recoverableException != null && recoverableException.isRecoverable()) {
                           return;
                         }
 
-                        if (e instanceof SdkClientException && ((SdkClientException) e).retryable()) {
+                        final SdkClientException sdkClientException = ExceptionUtil.getCause(e, SdkClientException.class);
+                        if (sdkClientException != null && sdkClientException.retryable()) {
                           return;
                         }
 
